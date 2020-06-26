3438449aa11c4326a3959861a53141c6c246b21f
ArmenMesropyan <mesropyan200315@gmail.com>
2020-06-26 16:52:22 +0400

    Search panel added with stylization and input change event listener

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Search.scss';
+import {Container} from 'reactstrap';
+
+const Search = ({onInputChange}) => {
+    return (
+        <section className="movies-page__search search-panel">
+            <Container>
+                <fieldset className="search-panel__fieldset">
+                    <i className="fa fa-search search-panel__icon"></i>
+                    <label htmlFor="search" className="visually-hidden">Search</label>
+                    <input type="text" className="search-panel__input" id="search" placeholder="Search" onChange={onInputChange}/>
+                </fieldset>
+            </Container>
+        </section>
+    )
+}
+
+export default Search;
