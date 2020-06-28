028ccbd8f03074f25df3029d1ac080f885cac203
ArmenMesropyan <mesropyan200315@gmail.com>
2020-06-28 21:53:04 +0400

    Trailers list component added with youtube player and slider

--- src/components/TrailersList/TrailersList.jsx
+++ src/components/TrailersList/TrailersList.jsx
@@ -1,47 +1,35 @@
 import React from 'react';
 import Slider from 'react-slick';
 import {Container} from 'reactstrap';
+import ReactPlayer from 'react-player'
 import './TrailersList.scss';
 
 const TrailersList = ({trailers}) => {
     if(!trailers || !trailers.length) return null;
-    console.log('trailers: ', trailers);
 
     const youtubeLinks = trailers.filter(item => item.site === 'YouTube')
-                                 .map(({key}) => `https://www.youtube.com/watch?v=${key}`);
-    console.log('youtubeLinks: ', youtubeLinks);
+                                 .map(({key}) => ({link: `https://www.youtube.com/watch?v=${key}?enablejsapi=1&origin=http://localhost:3000`, key}));
+    
+    const elements = youtubeLinks.map(({link, key}) => (
+        <li className="trailers__item" key={key}>
+            <ReactPlayer url={link} className="trailers__video"/>
+        </li>
+    ))
 
     const settings = {
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
         speed: 1000,
         autoplaySpeed: 2000,
     };
 
     return (
         <section className="trailers">
-            <Container>
+            <Container tag="ul" className="trailers__list">
+                <h2 className="trailers__title">Trailers</h2>
                 <Slider {...settings}>
-                <div>
-                    <h3>1</h3>
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-                <div>
-                    <h3>4</h3>
-                </div>
-                <div>
-                    <h3>5</h3>
-                </div>
-                <div>
-                    <h3>6</h3>
-                </div>
+                    {elements}
                 </Slider>
             </Container>
         </section>
